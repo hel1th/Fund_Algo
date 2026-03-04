10c495bccf5819b9c5d4d494490eb1e37b1c55f4
Vladimir Antygin <vl.antygin@gmail.com>
2026-03-04 21:22:29 +0300

    feat(splay): implement node addi, remove, and splay operations

--- TreeDataStructures/Implementations/Splay/SplayTree.cs
+++ TreeDataStructures/Implementations/Splay/SplayTree.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using TreeDataStructures.Implementations.BST;
+﻿using TreeDataStructures.Implementations.BST;
 
 namespace TreeDataStructures.Implementations.Splay;
 
@@ -11,17 +10,85 @@ public class SplayTree<TKey, TValue> : BinarySearchTree<TKey, TValue>
 
     protected override void OnNodeAdded(BstNode<TKey, TValue> newNode)
     {
-        throw new NotImplementedException();
+        Splay(newNode);
+    }
+
+    protected override void RemoveNode(BstNode<TKey, TValue> node)
+    {
+        Splay(node);
+        var (left, right) = Split(node);
+        Merge(left, right);
     }
 
     protected override void OnNodeRemoved(BstNode<TKey, TValue>? parent, BstNode<TKey, TValue>? child)
     {
-        throw new NotImplementedException();
     }
 
-    public override bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+    protected override void OnNodeAccessed(BstNode<TKey, TValue> node)
     {
-        throw new NotImplementedException();
+        Splay(node);
     }
 
+    private void Splay(BstNode<TKey, TValue> node)
+    {
+        while (node.Parent is not null)
+        {
+            var parent = node.Parent;
+            var gparent = parent.Parent;
+
+            if (gparent is null)
+            {
+                if (node.IsLeftChild)
+
+                    RotateRight(parent);
+                else
+                    RotateLeft(parent);
+            }
+            else
+            {
+                if (node.IsLeftChild && parent.IsLeftChild)
+                    RotateDoubleRight(gparent);
+                else if (node.IsLeftChild && parent.IsRightChild)
+                    RotateBigLeft(gparent);
+                else if (node.IsRightChild && parent.IsLeftChild)
+                    RotateBigRight(gparent);
+                else
+                    RotateDoubleLeft(gparent);
+            }
+        }
+
+        Root = node;
+    }
+
+    private (BstNode<TKey, TValue>? Left, BstNode<TKey, TValue>? Right) Split(BstNode<TKey, TValue> node)
+    {
+        var left = node.Left;
+        var right = node.Right;
+
+        left?.Parent = null;
+        right?.Parent = null;
+
+        return (left, right);
+    }
+
+    private void Merge(BstNode<TKey, TValue>? left, BstNode<TKey, TValue>? right)
+    {
+        if (left is null)
+        {
+            Root = right;
+            return;
+        }
+
+        if (right is null)
+        {
+            Root = left;
+            return;
+        }
+
+        var leftMax = GoRight(left);
+        Splay(leftMax);
+        leftMax.Right = right;
+        right.Parent = leftMax;
+        Root = leftMax;
+    }
 }
